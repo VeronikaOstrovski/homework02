8b011d9d5c357e58c7dfe9cf1aad38621fb495c6
veronikaostrovski <veronika.turkey@gmail.com>
2023-05-27 01:08:59 +0300

    Renamed classes

--- src/Number.java
+++ src/Number.java
@@ -1,4 +1,4 @@
-public class Main2 {
+public class Number {
     public static void main(String[] args) {
 
         System.out.println(12);
--- src/Star.java
+++ src/Star.java
@@ -1,4 +1,4 @@
-public class Main1 {
+public class Star {
     public static void main(String[] args) {
 
         System.out.println("☆");
